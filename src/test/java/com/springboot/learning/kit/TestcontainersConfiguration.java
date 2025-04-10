efa5ebb552ec504f7da86c202183173cec6c2381
Waleed Tariq <waleed.tariq192@gmail.com>
2025-04-11 00:21:43 +0100

    Initialising SpringBoot project

---
+++ src/test/java/com/springboot/learning/kit/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package com.springboot.learning.kit;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.activemq.ActiveMQContainer;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	ActiveMQContainer activemqContainer() {
+		return new ActiveMQContainer(DockerImageName.parse("apache/activemq-classic:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	MSSQLServerContainer<?> sqlServerContainer() {
+		return new MSSQLServerContainer<>(DockerImageName.parse("mcr.microsoft.com/mssql/server:latest"));
+	}
+
+}
